878ae300f991c5d829a5ca287c516e2754807dcd
Colin Piper <colin_piper@hotmail.co.uk>
2025-06-13 09:18:27 +0100

    fix: add skeleton classes

--- ui/amazon/pages/basketPage.ts
+++ ui/amazon/pages/basketPage.ts
@@ -1,3 +1,10 @@
+import { Locator } from "playwright/test";
+import { PlaywrightPage } from "../../../shared/pages/playwrightPage";
+
+export class BasketPage extends PlaywrightPage {
+    
+}
+
 // get total
 // increase items by
 // decrease items by
